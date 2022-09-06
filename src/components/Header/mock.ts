0ed35d84a52e2cec7e6c5e61a4828e90fa9c123f
Guilhermeaka <guilhermedssantos1@gmail.com>
2022-09-06 17:21:13 -0300

    Implementação de Componente Header

---
+++ src/components/Header/mock.ts
@@ -0,0 +1,8 @@
+import { HeaderProps } from '.';
+
+export default {
+  blogName: 'Guilherme dos Santos',
+  blogDescription: 'Uma descrição do Blog',
+  logo: 'assets/images/guilherme.jfif',
+  showText: true,
+} as HeaderProps;
